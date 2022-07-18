0902fdf9bf8b06257870e8ab5d1b13d69fa180ce
竹本廉 <takemotoren@takemotorennoMacBook-Air.local>
2022-07-19 03:24:46 +0900

    プロフィール作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :home
+  get 'users/:id/profile' => "users#profile"
+  post "users/:id/add" => "users#add"
+
   
   root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
