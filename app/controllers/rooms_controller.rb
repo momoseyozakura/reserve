04256370fdbd1adb65228a0bb099cfc3bf7d51f8
竹本廉 <takemotoren@takemotorennoMacBook-Air.local>
2022-07-24 00:03:33 +0900

    登録ルーム一覧作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,23 @@
 class RoomsController < ApplicationController
   def index
+    @rooms = Room.all
   end
   
   def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to("/")
+    else
+      render :new
+    end
+  end
+
+  private
+  def room_params
+    params.permit(:name, :memo , :fee , :area , :image)
   end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,9 @@
 class Room < ApplicationRecord
+  validates :name, presence: true
+  validates :memo, presence: true
+  validates :fee, presence: true
+  validates :area, presence: true
+  validates :image, presence: true
+
+  mount_uploader :image, ImageUploader
 end
