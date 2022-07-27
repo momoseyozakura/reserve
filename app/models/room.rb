85d9301895db5f99ef540be7c2a2e9559f7c86a6
竹本廉 <takemotoren@takemotorennoMacBook-Air.local>
2022-07-27 15:23:14 +0900

    予約フォーム修正

--- app/models/room.rb
+++ app/models/room.rb
@@ -7,6 +7,7 @@ class Room < ApplicationRecord
   validates :start_date, presence: true, on: :update
   validates :end_date, presence: true, on: :update
   validates :number, presence: true, on: :update
+  validate :date_check
   
   mount_uploader :image, ImageUploader
 
@@ -17,4 +18,12 @@ class Room < ApplicationRecord
   def sum_price
     number * fee * (end_date - start_date)
   end
+
+  def date_check
+    return false if self.end_date.blank? || self.start_date.blank?
+    if self.start_date > self.end_date
+        errors.add(:end_date,"は開始日より前の日付で登録できません")
+    end
+    
+end
 end
